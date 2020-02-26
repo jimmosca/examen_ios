1b0b933a61d573199f54045b9e4cda1ec8029ffa
Jaime Casado <jaime.casado.a@gmail.com>
2020-02-26 11:31:34 +0100

    root commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'examen_ios' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for examen_ios
+pod 'Firebase', '~> 6.1.0'
+pod 'Firebase/Analytics'
+pod 'Firebase/Auth'
+pod 'Firebase/Core'
+pod 'Firebase/Firestore'
+pod 'GoogleSignIn', '~> 4.1.0'
+
+end
---
+++ examen_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  examen_ios
+//
+//  Created by Jaime Casado Aparicio on 26/2/20.
+//  Copyright © 2020 Jaime Casado Aparicio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
